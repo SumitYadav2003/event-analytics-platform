fb9ad9dda5f06a17f99fef2c943f18e69354b590
Sumit Yadav <sumityadav8976502030@gmail.com>
2026-03-18 23:05:00 +0000

    feat: add application-based filtering to analytics endpoints

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -7,6 +7,16 @@ from applications.models import Application
 from events.models import Event
 
 
+def filter_events_by_application(request):
+    application_name = request.GET.get('application')
+    events = Event.objects.all()
+
+    if application_name:
+        events = events.filter(application__name=application_name)
+
+    return events, application_name
+
+
 @api_view(['GET'])
 def health_check(request):
     return Response({
@@ -17,11 +27,18 @@ def health_check(request):
 
 @api_view(['GET'])
 def analytics_summary(request):
-    total_events = Event.objects.count()
-    total_unique_users = Event.objects.values('user_id').distinct().count()
-    total_applications = Application.objects.count()
+    events, application_name = filter_events_by_application(request)
+
+    total_events = events.count()
+    total_unique_users = events.values('user_id').distinct().count()
+
+    if application_name:
+        total_applications = 1 if Application.objects.filter(name=application_name).exists() else 0
+    else:
+        total_applications = Application.objects.count()
 
     return Response({
+        "application": application_name,
         "total_events": total_events,
         "total_unique_users": total_unique_users,
         "total_applications": total_applications
@@ -30,24 +47,34 @@ def analytics_summary(request):
 
 @api_view(['GET'])
 def events_by_type(request):
+    events, application_name = filter_events_by_application(request)
+
     data = (
-        Event.objects
+        events
         .values('event_type')
         .annotate(count=Count('id'))
         .order_by('-count')
     )
 
-    return Response(list(data))
+    return Response({
+        "application": application_name,
+        "results": list(data)
+    })
 
 
 @api_view(['GET'])
 def events_per_day(request):
+    events, application_name = filter_events_by_application(request)
+
     data = (
-        Event.objects
+        events
         .annotate(day=TruncDate('timestamp'))
         .values('day')
         .annotate(count=Count('id'))
         .order_by('day')
     )
 
-    return Response(list(data))
+    return Response({
+        "application": application_name,
+        "results": list(data)
+    })
